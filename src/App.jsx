c92d5bdbac5b18530594909073c94b3bf74e016b
unknown <loc842211@gmail.com>
2025-07-13 10:19:22 +0700

    deploy vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import FavoritesPage from "./pages/FavoritesPage";
+import SearchResults from "./pages/SearchResults";
+import ChatBot from "./components/ChatBot";
+import { useAppContext } from "./context/AppContext";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
-import SearchResults from "./pages/SearchResults";
-const App = () => (
-  <>
 
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-      <Route path="/search" element={<SearchResults />} />
-    </Routes>
-    <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-    />
-  </>
-);
+const App = () => {
+  const { cartFavorite, setCartFavorite } = useAppContext();
+
+  const handleToggleFavorite = (id) => {
+    if (cartFavorite.includes(id)) {
+      setCartFavorite(cartFavorite.filter((f) => f !== id));
+    } else {
+      setCartFavorite([...cartFavorite, id]);
+    }
+  };
+
+  const handleViewDetail = (product) => {
+
+  };
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+
+      <ChatBot
+        onViewDetail={handleViewDetail}
+        onToggleFavorite={handleToggleFavorite}
+        favorites={cartFavorite}
+      />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
+};
+
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,23 +3,11 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import ProductList from '../components/ProductList';
 import ViewHistory from '../components/ViewHistory';
-import ChatBot from '../components/ChatBot';
-import { useAppContext } from "../context/AppContext";
 import { Container } from '@mui/material';
-import { useState } from 'react'
-const Home = () => {
-  const { cartFavorite, setCartFavorite } = useAppContext();
-  const [searchTerm, setSearchTerm] = useState("");
-  const handleToggleFavorite = (id) => {
-    if (cartFavorite.includes(id)) {
-      setCartFavorite(cartFavorite.filter((f) => f !== id));
-    } else {
-      setCartFavorite([...cartFavorite, id]);
-    }
-  };
+import { useState } from 'react';
 
-  const handleViewDetail = (product) => {
-  };
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState("");
 
   return (
     <>
@@ -30,12 +18,6 @@ const Home = () => {
         <ViewHistory />
       </Container>
       <Footer />
-
-      <ChatBot
-        onViewDetail={handleViewDetail}
-        onToggleFavorite={handleToggleFavorite}
-        favorites={cartFavorite}
-      />
     </>
   );
 };
